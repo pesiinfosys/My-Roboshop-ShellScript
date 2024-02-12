c524a1d47151f5122770cbd18f56a50163dcb6a9
pesi <pesiinfosys@gmail.com>
2024-02-12 06:15:03 +0000

    adding 12-Create-EC2.sh file

---
+++ 12-Create-EC2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Variable Declaration
+LOGDIR=/tmp
+DATE=$(date +%F::%H:%M)
+SCRIPT=$0
+LOGFILE=$LOGDIR/$SCRIPT-$DATE.log
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+# Function Declaration
+VALIDATION(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 Was.... $R FAIL $N"
+        exit 1
+    else
+        echo -e "$2 Was.... $G SUCCESS $N"
+    fi
+}
+
+# Main Section
+if [ $USERID -ne 0 ];
+then 
+    echo -e "$Y ERROR:$N... Need root privilages" 
+    exit 1 
+fi
+echo -e "Executing The Script... $Y For log verification check $LOGFILE $N"
+echo -e "$B###############################################$N" > $LOGFILE
+echo -e "$Y Script Execution At: $DATE by $(whoami)" &>> $LOGFILE
+echo -e "$B###############################################$N" >> $LOGFILE
+
+INSTANCES=("MongoDB" "Redis" "MySQL" "RabbitMQ" "Catalogue" "Cart" "User" "Shipping" "Payment" "Dispatch" "Roboshop-WebServer")
+
+for i in ${INSTANCES[@]}
+do 
+    echo "Creating Instance : $B $i $N"
+done
